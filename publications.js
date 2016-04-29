43c54a2a947cce70121c871d68c8fb87115af17f
lucyrichards <lucyannerichards@gmail.com>
2016-04-28 18:37:16 -0700

    es6-ified publications.js

--- publications.js
+++ publications.js
@@ -4,27 +4,37 @@ import { Lessons } from "meteor/mongo-schemas";
 import { Modules } from "meteor/mongo-schemas";
 import { OfflineFiles } from "meteor/mongo-schemas";
 
-Meteor.publish("modules.inLesson", (lessonId) =>
+Meteor.publish("modules.inLesson", function(lessonId) {
   if( !lessonId ) {
     return [];
   }
   let lesson = Lessons.findOne( {_id: lessonId} );
   return Modules.find( {_id: {$in: lesson.modules}} );
-);
+});
 
-Meteor.publish "curriculums.all", ->
-  return Curriculums.find({})
+Meteor.publish("curriculums.all", function() {
+  return Curriculums.find({});
+});
 
-Meteor.publish "curriculum", (id) ->
-  if id then Curriculums.find ({_id: id}) else []
-
-Meteor.publish "lessons.inCurriculum", (curriculumId) ->
-  if curriculumId
-    curr = Curriculums.findOne {_id: curriculumId}
-    if curr? then return Lessons.find {_id: {$in: curr.lessons}} else []
+Meteor.publish("curriculums", function(id) {
+  if( id )
+    return Curriculums.find ({_id: id});
   else
-    return []
+    return [];
+});
 
-Meteor.publish "lesson", (id) ->
-  return Lessons.find ({_id: id})
+Meteor.publish("lessons.inCurriculum", function(curriculumId) {
+  if( curriculumId ) {
+    let curr = Curriculums.findOne( {_id: curriculumId} );
+    if( curr )
+      return Lessons.find( {_id: {$in: curr.lessons}} );
+  }
+
+  return [];
+});
+
+
+Meteor.publish( "lesson", function(id) {
+  return Lessons.find({_id: id});
+});
 
