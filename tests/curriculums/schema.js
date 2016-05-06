6c62804530401fe23a696edf289eb45e4ea09f0d
lucyrichards <lucyannerichards@gmail.com>
2016-05-06 16:29:31 -0700

    checks for types of errors in callback

--- tests/curriculums/schema.js
+++ tests/curriculums/schema.js
@@ -37,6 +37,8 @@ describe("Curriculum Schema", ()=> {
     Curriculums.insert( noTitle, function(error, id){
       setTimeout(function(){
         should.not.equal(null, error);
+        should.equal(error.invalidKeys.length, 1);
+        should.equal(error.invalidKeys[0].name, "title");
         should.equal(false, id);
         done();
       });
@@ -57,6 +59,8 @@ describe("Curriculum Schema", ()=> {
     Curriculums.insert( titleTooLong, function(error, id){
       setTimeout(function(){
         should.not.equal(null, error);
+        should.equal(error.invalidKeys.length, 1);
+        should.equal(error.invalidKeys[0].name, "title");
         should.equal(false, id);
         done();
       });
@@ -72,6 +76,8 @@ describe("Curriculum Schema", ()=> {
     Curriculums.insert( shouldBeAnArray, function(error, id){
       setTimeout(function(){
         should.not.equal(null, error);
+        should.equal(error.invalidKeys.length, 1);
+        should.equal(error.invalidKeys[0].name, "lessons");
         should.equal(false, id);
         done();
       });
@@ -87,10 +93,11 @@ describe("Curriculum Schema", ()=> {
     Curriculums.insert( noTitle, function(error, id){
       setTimeout(function(){
         should.not.equal(null, error);
+        should.equal(error.invalidKeys.length, 1);
+        should.equal(error.invalidKeys[0].name, "title");
         should.equal(false, id);
         done();
       });
     });
   });
-
 });
