829192ae91f1e3b1bc9d1dfad9206394d29fa656
lucyrichards <lucyannerichards@gmail.com>
2016-05-06 17:45:41 -0700

    video-validation is passing

--- tests/modules/video-validation.js
+++ tests/modules/video-validation.js
@@ -8,7 +8,8 @@ describe("Modules Video Validation", ()=> {
 
   it("Should reject the doc when the video has a colon in the name", (done) => {
     let withColon = {
-      type: "SCENARIO",
+      type: "VIDEO",
+      title: "Title",
       video: "video:something.mp4"
     };
 
@@ -16,6 +17,8 @@ describe("Modules Video Validation", ()=> {
     Modules.insert( withColon, function(error, id){
       setTimeout(function(){
         should.not.equal(null, error);
+        should.equal("video", error.invalidKeys[0].name);
+        should.equal( error.invalidKeys.length, 1);
         should.equal(false, id);
         done();
       });
@@ -24,7 +27,8 @@ describe("Modules Video Validation", ()=> {
 
   it("Should reject the doc when the video has a space in the name", (done) => {
     let withSpace = {
-      type: "SCENARIO",
+      type: "VIDEO",
+      title: "Title",
       video: "video something.mp4"
     };
 
@@ -32,6 +36,8 @@ describe("Modules Video Validation", ()=> {
     Modules.insert( withSpace, function(error, id){
       setTimeout(function(){
         should.not.equal(null, error);
+        should.equal("video", error.invalidKeys[0].name);
+        should.equal( error.invalidKeys.length, 1);
         should.equal(false, id);
         done();
       });
@@ -40,13 +46,16 @@ describe("Modules Video Validation", ()=> {
 
   it("Should reject the doc when the video has a underscore in the name", (done) => {
     let withUnderscore = {
-      type: "SCENARIO",
+      type: "VIDEO",
+      title: "title",
       video: "video_with__underscore.mp4"
     };
 
     Modules.insert( withUnderscore, function(error, id){
       setTimeout(function(){
         should.not.equal(null, error);
+        should.equal("video", error.invalidKeys[0].name);
+        should.equal( error.invalidKeys.length, 1);
         should.equal(false, id);
         done();
       });
@@ -55,13 +64,16 @@ describe("Modules Video Validation", ()=> {
 
   it("Should reject the doc when the video has a dash in the name", (done) => {
     let withDash = {
-      type: "BINARY",
+      type: "VIDEO",
+      title: "title",
       video: "video-something.mp4"
     };
 
     Modules.insert( withDash, function(error, id){
       setTimeout(function(){
         should.not.equal(null, error);
+        should.equal("video", error.invalidKeys[0].name);
+        should.equal( error.invalidKeys.length, 1);
         should.equal(false, id);
         done();
       });
@@ -70,7 +82,8 @@ describe("Modules Video Validation", ()=> {
 
   it("Should accept video files with numbers in the name", (done) => {
     let numbers = {
-      type: "MULTIPLE_CHOICE",
+      type: "VIDEO",
+      title: "title",
       video: "1324234.mp4"
     };
 
@@ -85,7 +98,8 @@ describe("Modules Video Validation", ()=> {
 
   it("Should accept video of type .mov", (done) => {
     let mov = {
-      type: "MULTIPLE_CHOICE",
+      type: "VIDEO",
+      title: "title",
       video: "video.mov"
     };
 
@@ -100,7 +114,8 @@ describe("Modules Video Validation", ()=> {
 
   it("Should accept video of type .mp4", (done) => {
     let mp4 = {
-      type: "MULTIPLE_CHOICE",
+      type: "VIDEO",
+      title: "title",
       video: "video.mp4"
     };
 
